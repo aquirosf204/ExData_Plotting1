7beb419fe11b448d9a3c011880b021b4aeee0a48
aquirosf204 <andreaqf.10@gmail.com>
2025-01-09 21:36:50 +0000

    Update Plot1 AQF.R

--- Plot1 AQF.R
+++ Plot1 AQF.R
@@ -13,5 +13,5 @@ house_pc_subset <- house_pc[house_pc$Date >= "2007-02-01" & house_pc$Date <= "20
 par(mfrow = c(1,1))
 hist(house_pc_subset$Global_active_power, col = "red", main = paste("Global Active Power"),
      xlab = "Global Active Power (kilowatts)")
-dev.copy(png, file="plot1.png", width=480, height=480)
+dev.copy(png, file="plot1 AQF.png", width=480, height=480)
 dev.off()
