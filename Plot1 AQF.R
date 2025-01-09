0c0d627271a8a1d73eeb1895a3cde104de10f10e
aquirosf204 <andreaqf.10@gmail.com>
2025-01-09 21:25:32 +0000

    Add files via upload

---
+++ Plot1 AQF.R
@@ -0,0 +1,17 @@
+
+library(readr)
+house_pc <- read.csv("C:/Users/andre/Downloads/R/Coursera/data_p_comp/household_power_consumption.txt", sep=";")
+
+house_pc$Global_active_power <- as.numeric(house_pc$Global_active_power)
+house_pc <- house_pc[!is.na(house_pc$Global_active_power), ]
+
+house_pc$Date <- as.Date(house_pc$Date, format="%d/%m/%Y")
+
+# Subset data for 01/02/2007 to 02/02/2007
+house_pc_subset <- house_pc[house_pc$Date >= "2007-02-01" & house_pc$Date <= "2007-02-02", ]
+
+par(mfrow = c(1,1))
+hist(house_pc_subset$Global_active_power, col = "red", main = paste("Global Active Power"),
+     xlab = "Global Active Power (kilowatts)")
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
